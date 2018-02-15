b896c3cc9e23666f72c3004876d8beec9ca3889b
Navie Vurdien <nvurdien@gmail.com>
2018-02-15 00:23:22 -0800

    commented out socials

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,9 +8,9 @@ const colorToWhite = {
 export const Footer = () => (
     <div className="uk-section uk-section-secondary uk-padding-small uk-text-center">
         <br />
-        <p><a href="https://www.twitter.com/acmcsuf" aria-label="twitter link" target="_blank" rel="noopener noreferrer" title="@acmcsuf" className="uk-icon-button uk-margin-small-right" uk-icon="icon: twitter"/>
-            <a href="https://www.facebook.com/groups/acmcsuf" aria-label="facebook group link" rel="noopener noreferrer" target="_blank" title="/group/acmcsuf" className="uk-icon-button uk-margin-small-right" uk-icon="icon: facebook"/>
-            <a href="https://www.instagram.com/acmcsuf" aria-label="instagram link" target="_blank" rel="noopener noreferrer" title="@acmcsuf" className="uk-icon-button uk-margin-small-right" uk-icon="icon: instagram"/></p>
+        {/*<p><a href="https://www.twitter.com/acmcsuf" aria-label="twitter link" target="_blank" rel="noopener noreferrer" title="@acmcsuf" className="uk-icon-button uk-margin-small-right" uk-icon="icon: twitter"/>*/}
+            {/*<a href="https://www.facebook.com/groups/acmcsuf" aria-label="facebook group link" rel="noopener noreferrer" target="_blank" title="/group/acmcsuf" className="uk-icon-button uk-margin-small-right" uk-icon="icon: facebook"/>*/}
+            {/*<a href="https://www.instagram.com/acmcsuf" aria-label="instagram link" target="_blank" rel="noopener noreferrer" title="@acmcsuf" className="uk-icon-button uk-margin-small-right" uk-icon="icon: instagram"/></p>*/}
         <p style={colorToWhite}>ECS Leadership and Diversity Summit | Spring 2018</p>
         <br />
     </div>
