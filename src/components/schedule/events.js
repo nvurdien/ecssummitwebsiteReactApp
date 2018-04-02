d9ed81a279ab2d395fd683f7d3f1b4fb11526ff5
chrisvillan <villanuevachris19@gmail.com>
2018-04-01 23:45:40 -0700

    updated from upstream branch and added to event.js file

--- src/components/schedule/events.js
+++ src/components/schedule/events.js
@@ -1,5 +1,7 @@
 export default [
   {
+
+// Friday April 6th
     "title": "Check-In - SGMH 1303",
     "id": "821",
     "start": "2018-04-06 13:00:00",
@@ -10,5 +12,105 @@ export default [
     "id": "822",
     "start": "2018-04-06 13:30:00",
     "end": "2018-04-06 13:40:00"
+  },
+  {
+    "title": "Active Listening Workshop - SGMH 1307",
+    "id": "822",
+    "start": "2018-04-06 14:00:00",
+    "end": "2018-04-06 14:40:00"
+  },
+  {
+    "title": "Leadership Keynote - SGMH 1307",
+    "id": "822",
+    "start": "2018-04-06 14:50:00",
+    "end": "2018-04-06 15:50:00"
+  },
+  {
+    "title": "Day 1 Speed Mentoring - SGMH 1108",
+    "id": "822",
+    "start": "2018-04-06 16:00:00",
+    "end": "2018-04-06 17:30:00"
+  },
+  {
+    "title": "Day 1 Debrief - SGMH 1307",
+    "id": "822",
+    "start": "2018-04-06 17:40:00",
+    "end": "2018-04-06 18:00:00"
+  },
+  {
+    "title": "Social - TSU Bowl & Billiards",
+    "id": "822",
+    "start": "2018-04-06 18:00:00",
+    "end": "2018-04-06 20:00:00"
+  },
+
+
+// Saturday April 7th
+  {
+    "title": "Breakfast - SGMH 1303",
+    "id": "821",
+    "start": "2018-04-07 8:30:00",
+    "end": "2018-04-07 9:30:00"
+  },
+  {
+    "title": "Check-In - SGMH 1303",
+    "id": "821",
+    "start": "2018-04-07 9:30:00",
+    "end": "2018-04-07 10:10:00"
+  },
+  {
+    "title": "Welcome Back - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 10:10:00",
+    "end": "2018-04-07 11:00:00"
+  },
+  {
+    "title": "Strengthsfinder Test - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 10:10:00",
+    "end": "2018-04-07 11:00:00"
+  },
+  {
+    "title": "Strengthsfinder Presentation - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 11:10:00",
+    "end": "2018-04-07 11:50:00"
+  },
+  {
+    "title": "Out of This World Activity - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 12:00:00",
+    "end": "2018-04-07 12:40:00"
+  },
+  {
+    "title": "Lunch - SGMH 1303",
+    "id": "821",
+    "start": "2018-04-07 12:50:00",
+    "end": "2018-04-07 13:50:00"
+  },
+  {
+    "title": "Keynote Speaker - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 14:00:00",
+    "end": "2018-04-07 14:30:00"
+  },
+  {
+    "title": "Panel Session - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 14:40:00",
+    "end": "2018-04-07 15:50:00"
+  },
+  {
+    "title": "Day 2 Speed mentoring - SGMH 2201 & SGMH 2301",
+    "id": "821",
+    "start": "2018-04-07 16:00:00",
+    "end": "2018-04-07 17:30:00"
+  },
+  {
+    "title": "Closing Reception - SGMH 1307",
+    "id": "821",
+    "start": "2018-04-07 17:30:00",
+    "end": "2018-04-07 18:00:00"
   }
+
 ]
